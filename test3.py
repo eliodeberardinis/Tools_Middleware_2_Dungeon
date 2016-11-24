7ef87845e543497786cc53866d2cefafbeb466b4
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2016-11-24 16:26:53 +0100

    Test 3 now displays a box for the player
    Added a box with the size of a player following convention (128x32
    units), in order to have a sense of the size of the dungeon.

--- test3.py
+++ test3.py
@@ -36,6 +36,65 @@ def copyTile(index, manager, scene, nodeName = "", meshName = ""):
     return newNode
 
 
+def makeBox(width, height, depth, manager, nodeName = "", meshName = ""):
+    width *= 0.5
+    depth *= 0.5
+    newNode = FbxNode.Create(manager, nodeName)
+    newMesh = FbxMesh.Create(manager, meshName)
+
+    #Copy the vertices in the mesh
+    newMesh.InitControlPoints(8)
+    newMesh.SetControlPointAt(FbxVector4(width, 0.0, depth, 0.0), 0)
+    newMesh.SetControlPointAt(FbxVector4(width, 0.0, -depth, 0.0), 1)
+    newMesh.SetControlPointAt(FbxVector4(-width, 0.0, -depth, 0.0), 2)
+    newMesh.SetControlPointAt(FbxVector4(-width, 0.0, depth, 0.0), 3)
+    newMesh.SetControlPointAt(FbxVector4(width, height, depth, 0.0), 4)
+    newMesh.SetControlPointAt(FbxVector4(width, height, -depth, 0.0), 5)
+    newMesh.SetControlPointAt(FbxVector4(-width, height, -depth, 0.0), 6)
+    newMesh.SetControlPointAt(FbxVector4(-width, height, depth, 0.0), 7)
+    newMesh.BeginPolygon(); #bottom
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(3);
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(1);
+    newMesh.EndPolygon();
+    newMesh.BeginPolygon(); #top
+    newMesh.AddPolygon(4);
+    newMesh.AddPolygon(5);
+    newMesh.AddPolygon(6);
+    newMesh.AddPolygon(7);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #front
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(4);
+    newMesh.AddPolygon(7);
+    newMesh.AddPolygon(3);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #back
+    newMesh.AddPolygon(1);
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(6);
+    newMesh.AddPolygon(5);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #right
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(1);
+    newMesh.AddPolygon(5);
+    newMesh.AddPolygon(4);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #left
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(3);
+    newMesh.AddPolygon(7);
+    newMesh.AddPolygon(6);
+    newMesh.EndPolygon();
+    
+    #Add the mesh to the node
+    newNode.SetNodeAttribute(newMesh)
+
+    return newNode
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Error: no kit file path specified.")
@@ -79,6 +138,11 @@ if __name__ == "__main__":
                                         stairs.LclTranslation.Get()[1],
                                         stairs.LclTranslation.Get()[2] + 1500))
 
+    box = makeBox(32, 128, 32, manager, "Player", "PlayerMesh")
+    scene2.GetRootNode().AddChild(box)
+    box.LclTranslation.Set(FbxDouble3(box.LclTranslation.Get()[0],
+                                      box.LclTranslation.Get()[1],
+                                      box.LclTranslation.Get()[2] - 1125))
     #Save the scene in a new file
     if len(sys.argv) > 2:
         print("Saving the scene into '%s'." % sys.argv[2])
