810dd789b67b724039195ef7b2e7556f071e1d37
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2016-12-01 16:17:42 +0100

    Implemented concatenation for more tiles.
    Implemented the concatenation for all 2-way tiles (3 and 4 way tiles to do).

--- DungeonProject/DungeonProject/DungeonProject.py
+++ DungeonProject/DungeonProject/DungeonProject.py
@@ -15,9 +15,28 @@ The three first values are the x, y and z coordinate values.
 The fourth value is the rotation about the Y axis.
 '''
 tiles = [
-    [36, [0, 0, 400, 0], [0, 0, 800, 0]], #narrow hallway
-    [37, [0, 0, 200, 0], [200, 0, 200, -90]], #narrow corner right
-    [37, [200, 0, 0, 90], [-200, 0, 200, 90]] #narrow corner left
+    [36, [0, 0, 400, 0], [0, 0, -800, 0]], #narrow hallway
+    [37, [0, 0, 200, 0], [200, 0, -200, -90]], #narrow corner right
+    [37, [200, 0, 0, 90], [-200, 0, -200, 90]], #narrow corner left
+    [17, [0, 0, 400, 0], [0, 0, -800, 0]], #wide hallway
+    [30, [0, 0, 400, 0], [400, 0, -400, -90]], #wide corner right
+    [30, [400, 0, 0, 90], [-400, 0, -400, 90]], #wide corner left
+    [6, [0, 0, 800, 0], [0, 0, -1600, 0]], #extrawide hallway
+    [5, [0, 0, 800, 0], [800, 0, -800, -90]], #extrawide corner right
+    [5, [800, 0, 0, 90], [-800, 0, -800, 90]], #extrawide corner left
+    [35, [0, 0, -400, 180], [0, -400, -1600, 0]], #narrow ramp down
+    [35, [0, -400, 1200, 0], [0, 400, -1600, 0]], #narrow ramp up
+    [18, [0, 0, -400, 180], [0, -400, -1600, 0]], #wide ramp down
+    [18, [0, -400, 1200, 0], [0, 400, -1600, 0]], #wide ramp up
+    [24, [0, 0, 0, 0], [0, 0, 0, 0]], #narrow door square-wide
+    [28, [0, 0, 0, 0], [0, 0, 0, 0]], #narrow door pointy
+    [27, [0, 0, 0, 0], [0, 0, 0, 0]], #narrow door square-narrow
+    [25, [0, 0, 0, 0], [0, 0, 0, 0]], #wide door square
+    [29, [0, 0, 0, 0], [0, 0, 0, 0]], #wide door square
+    [26, [0, 0, 0, 0], [0, 0, 0, 0]], #wide door square
+    [2, [0, 0, 0, 0], [0, 0, 0, 0]], #extrawide door square
+    [0, [0, 0, 0, 0], [0, 0, 0, 0]], #extrawide door square
+    [1, [0, 0, 0, 0], [0, 0, 0, 0]], #extrawide door square
 ]
 
 def copyTile(index, manager, scene, nodeName = "", meshName = ""):
@@ -124,9 +143,9 @@ def generateTile(index, manager, kitScene, scene, transform):
     tile.LclTranslation.Set(FbxDouble3(transform[0] + x * math.cos(math.radians(transform[3])) - z * math.sin(math.radians(transform[3])),
                                        transform[1] - tiles[index][1][1],
                                        transform[2] - z * math.cos(math.radians(transform[3])) + x * math.sin(math.radians(transform[3]))))
-    return [transform[0] + tiles[index][2][0] * math.cos(math.radians(transform[3])) - tiles[index][2][2] * math.sin(math.radians(transform[3])),
-            transform[1] - tiles[index][2][1],
-            transform[2] - tiles[index][2][2] * math.cos(math.radians(transform[3])) - tiles[index][2][0] * math.sin(math.radians(transform[3])),
+    return [transform[0] + tiles[index][2][0] * math.cos(math.radians(transform[3])) + tiles[index][2][2] * math.sin(math.radians(transform[3])),
+            transform[1] + tiles[index][2][1],
+            transform[2] + tiles[index][2][2] * math.cos(math.radians(transform[3])) - tiles[index][2][0] * math.sin(math.radians(transform[3])),
             transform[3] + tiles[index][2][3]]
 
 
@@ -149,20 +168,10 @@ if __name__ == "__main__":
     scene2 = FbxScene.Create(manager, '')
 
     currentTransform = [0, 0, 0, 0]
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(2, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(1, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(1, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(1, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(2, manager, scene, scene2, currentTransform)
-    currentTransform = generateTile(0, manager, scene, scene2, currentTransform)
-
+    sequence = [19, 6, 19, 0, 0, 1, 0, 1, 0, 2, 0, 2, 2, 1, 0, 13, 3, 5, 3, 11, 3, 5, 3, 19, 7, 19, 2, 0, 0, 10, 0, 0, 0, 14]
+    for i in sequence:
+        currentTransform = generateTile(i, manager, scene, scene2, currentTransform)
+    
     #Save the scene in a new file
     if len(sys.argv) > 2:
         print("Saving the scene into '%s'." % sys.argv[2])
