475ca5f77239c4c3e68d0e0e9721369d8a69ea4d
javolo <javo.osa@gmail.com>
2016-12-31 09:10:16 +0000

    Vector methos into math module class

--- DungeonProject/DungeonProject/MathModule.py
+++ DungeonProject/DungeonProject/MathModule.py
@@ -31,6 +31,45 @@ class MathModule:
             270: 0
             }.get(angle, math.cos(math.radians(angle)))
 
+    # Returns the eight corner points of the given BB
+    def getBBpoints(bb):
+        vectors = [
+            [[bb[1][0][0] * cos(bb[0][3]), 0, bb[1][0][0] * sin(bb[0][3])], 
+             [bb[1][0][1] * cos(bb[0][3]), 0, bb[1][0][1] * sin(bb[0][3])],],
+            [[0, bb[1][1][0], 0], [0, bb[1][1][1], 0]],
+            [[-bb[1][2][0] * sin(bb[0][3]), 0, bb[1][2][0] * cos(bb[0][3])], 
+             [-bb[1][2][1] * sin(bb[0][3]), 0, bb[1][2][1] * cos(bb[0][3])]],
+            ]
+
+        return [
+            addVectors([bb[0][:3], vectors[0][0], vectors[1][0], vectors[2][0]]),
+            addVectors([bb[0][:3], vectors[0][0], vectors[1][0], vectors[2][1]]),
+            addVectors([bb[0][:3], vectors[0][0], vectors[1][1], vectors[2][0]]),
+            addVectors([bb[0][:3], vectors[0][0], vectors[1][1], vectors[2][1]]),
+            addVectors([bb[0][:3], vectors[0][1], vectors[1][0], vectors[2][0]]),
+            addVectors([bb[0][:3], vectors[0][1], vectors[1][0], vectors[2][1]]),
+            addVectors([bb[0][:3], vectors[0][1], vectors[1][1], vectors[2][0]]),
+            addVectors([bb[0][:3], vectors[0][1], vectors[1][1], vectors[2][1]])
+        ]
+
+    # Adds an infinite amount of vectors
+    # PRE: all vectors must have the same dimension (behaviour undefined if not true)
+    def addVectors(vectors):
+        result = vectors[0]
+        for v in vectors[1:]:
+            for i in range(len(result)):
+                result[i] += v[i]
+        return result
+
+    # Creates the AABB that contains all points
+    def AABB(points):
+        points = zip(*points)
+        min_x = min(points[0])
+        max_x = max(points[0])
+        min_y = min(points[1])
+        max_y = max(points[1])
+        return (min_x, min_y, max_x - min_x, max_y - min_y)
+
 
 
 
