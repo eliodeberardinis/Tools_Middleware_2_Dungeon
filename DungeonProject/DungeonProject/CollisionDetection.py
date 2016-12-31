d045a765e5dda0d2cf2b2e5cf6afc224e93a9065
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2016-12-31 12:21:25 +0100

    Cleaned up module references.
    Changed modules from classes with all static methods to just modules with regular methods.
    Cleaned up references to methods in modules.

--- DungeonProject/DungeonProject/CollisionDetection.py
+++ DungeonProject/DungeonProject/CollisionDetection.py
@@ -1,53 +1,45 @@
-import sys
-import MathModule
+# This module contains the methods used for collision detection
+
 from MathModule import *
 
-class CollisionDetection:
+# Checks if the given BB overlaps with any of the of BB's in the list
+def checkCollision(bb1, bbs):
+    for bb in bbs:
+        if testCollision(bb1, bb):
+            return True
+    return False
 
-    # Checks if the given BB overlaps with any of the of BB's in the list
-    @staticmethod
-    def checkCollision(bb1, bbs):
-        for bb in bbs:
-            if CollisionDetection.testCollision(bb1, bb):
-                return True
-        return False
+# Checks if two BB's (Bounding Boxes) overlap
+# A BB is defined by: [centre, [sizeX, sizeY, sizeZ]]
+#   where centre is a transform (4D vector),
+#   and sizeN is a pair of two values (each one for each direction)
+# This collision test is based on checking collisions in the projections to the planes of each BB
+#   This is called the Separating Axis Theorem (reference: http://www.dyn4j.org/2010/01/sat/)
+def testCollision(bb1, bb2):
+    return testCollisionOnProjectionPlane(bb1[0][3], bb1, bb2) and \
+        testCollisionOnProjectionPlane(bb1[0][3] + 90, bb1, bb2) and \
+        testCollisionOnProjectionPlane(bb2[0][3], bb1, bb2) and \
+        testCollisionOnProjectionPlane(bb2[0][3] + 90, bb1, bb2)
 
-    # Checks if two BB's (Bounding Boxes) overlap
-    # A BB is defined by: [centre, [sizeX, sizeY, sizeZ]]
-    #   where centre is a transform (4D vector),
-    #   and sizeN is a pair of two values (each one for each direction)
-    # This collision test is based on checking collisions in the projections to the planes of each BB
-    #   This is called the Separating Axis Theorem (reference: http://www.dyn4j.org/2010/01/sat/)
-    @staticmethod
-    def testCollision(bb1, bb2):
-        return CollisionDetection.testCollisionOnProjectionPlane(bb1[0][3], bb1, bb2) and \
-            CollisionDetection.testCollisionOnProjectionPlane(bb1[0][3] + 90, bb1, bb2) and \
-            CollisionDetection.testCollisionOnProjectionPlane(bb2[0][3], bb1, bb2) and \
-            CollisionDetection.testCollisionOnProjectionPlane(bb2[0][3] + 90, bb1, bb2)
+# Checks if two BB's collide when projected onto the vertical origin-passing plane given by the angle
+def testCollisionOnProjectionPlane(angle, bb1, bb2):
+    # Get the corner points of each BB
+    bb1points = getBBpoints(bb1)
+    bb2points = getBBpoints(bb2)
 
-    # Checks if two BB's collide when projected onto the vertical origin-passing plane given by the angle
-    @staticmethod
-    def testCollisionOnProjectionPlane(angle, bb1, bb2):
-        # Get the corner points of each BB
-        bb1points = MathModule.getBBpoints(bb1)
-        bb2points = MathModule.getBBpoints(bb2)
+    # Project every point onto the plane
+    projbb1 = [projectPointOntoPlane(angle, point) for point in bb1points]
+    projbb2 = [projectPointOntoPlane(angle, point) for point in bb2points]
 
-        # Project every point onto the plane
-        projbb1 = [MathModule.projectPointOntoPlane(angle, point) for point in bb1points]
-        projbb2 = [MathModule.projectPointOntoPlane(angle, point) for point in bb2points]
+    # Check if the AABB's that contain the projected points collide
+    aabb1 = AABB(projbb1)
+    aabb2 = AABB(projbb2)
+    return checkCollisionAABB(aabb1, aabb2)
 
-        # Check if the AABB's that contain the projected points collide
-        aabb1 = MathModule.AABB(projbb1)
-        aabb2 = MathModule.AABB(projbb2)
-        return CollisionDetection.checkCollisionAABB(aabb1, aabb2)
-
-    # Checks if the two AABB's (Axis Aligned Bounding Box) collide
-    # Reference: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    @staticmethod
-    def checkCollisionAABB(aabb1, aabb2):
-        return aabb1[0] < aabb2[0] + aabb2[2] and \
-            aabb1[0] + aabb1[2] > aabb2[0] and \
-            aabb1[1] < aabb2[1] + aabb2[3] and \
-            aabb1[1] + aabb1[3] > aabb2[1]
-
-
+# Checks if the two AABB's (Axis Aligned Bounding Box) collide
+# Reference: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+def checkCollisionAABB(aabb1, aabb2):
+    return aabb1[0] < aabb2[0] + aabb2[2] and \
+        aabb1[0] + aabb1[2] > aabb2[0] and \
+        aabb1[1] < aabb2[1] + aabb2[3] and \
+        aabb1[1] + aabb1[3] > aabb2[1]
--- DungeonProject/DungeonProject/Utilities.py
+++ DungeonProject/DungeonProject/Utilities.py
@@ -1,92 +1,88 @@
+# This module contains methods for miscellaneous utilities used in the system.
+
 from fbx import *
-import sys
 import random
 
-class Utilities:
+# Splits a graph into its immediate branches
+# Examples:
+# split("XXXX") = ["XXXX"]
+# split("[X,XXX]") = ["X","XXX"]
+def split(graph):
+    if len(graph) == 0 or graph[0] != "[":
+        return [graph]
+    graphs = [""]
+    depth = 0
+    for c in graph[1:-1]:
+        if c == "," and depth == 0:
+            graphs += [""]
+        else:
+            graphs[-1] += c
+            depth += 1 if c == "[" else -1 if c == "]" else 0
+    return graphs
 
-    # Splits a graph into its immediate branches
-    # Examples:
-    # split("XXXX") = ["XXXX"]
-    # split("[X,XXX]") = ["X","XXX"]
-    @staticmethod
-    def split(graph):
-        if len(graph) == 0 or graph[0] != "[":
-            return [graph]
-        graphs = [""]
-        depth = 0
-        for c in graph[1:-1]:
-            if c == "," and depth == 0:
-                graphs += [""]
-            else:
-                graphs[-1] += c
-                depth += 1 if c == "[" else -1 if c == "]" else 0
-        return graphs
+# Returns a random key using the given weights
+# Data must be inputed as a dictionary, with choices as keys and weights as values.
+def randomWeightedChoice(data):
+    cumulative = [sum(data.values()[:i+1]) for i in range(len(data.values()))]
+    value = random.uniform(0, cumulative[-1])
+    index = next(i for i in range(len(cumulative)) if cumulative[i] > value)
+    return data.keys()[index]
 
-    # Returns a random key using the given weights
-    # Data must be inputed as a dictionary, with choices as keys and weights as values.
-    @staticmethod
-    def randomWeightedChoice(data):
-        cumulative = [sum(data.values()[:i+1]) for i in range(len(data.values()))]
-        value = random.uniform(0, cumulative[-1])
-        index = next(i for i in range(len(cumulative)) if cumulative[i] > value)
-        return data.keys()[index]
+# Creates a box mesh with the specified dimensions and returns a node that contains it
+def makeBox(width, height, depth, manager, nodeName = "", meshName = ""):
+    width *= 0.5
+    depth *= 0.5
+    newNode = FbxNode.Create(manager, nodeName)
+    newMesh = FbxMesh.Create(manager, meshName)
 
-    # Creates a box mesh with the specified dimensions and returns a node that contains it
-    @staticmethod
-    def makeBox(width, height, depth, manager, nodeName = "", meshName = ""):
-        width *= 0.5
-        depth *= 0.5
-        newNode = FbxNode.Create(manager, nodeName)
-        newMesh = FbxMesh.Create(manager, meshName)
-
-        #Copy the vertices in the mesh
-        newMesh.InitControlPoints(8)
-        newMesh.SetControlPointAt(FbxVector4(width, 0.0, depth, 0.0), 0)
-        newMesh.SetControlPointAt(FbxVector4(width, 0.0, -depth, 0.0), 1)
-        newMesh.SetControlPointAt(FbxVector4(-width, 0.0, -depth, 0.0), 2)
-        newMesh.SetControlPointAt(FbxVector4(-width, 0.0, depth, 0.0), 3)
-        newMesh.SetControlPointAt(FbxVector4(width, height, depth, 0.0), 4)
-        newMesh.SetControlPointAt(FbxVector4(width, height, -depth, 0.0), 5)
-        newMesh.SetControlPointAt(FbxVector4(-width, height, -depth, 0.0), 6)
-        newMesh.SetControlPointAt(FbxVector4(-width, height, depth, 0.0), 7)
-        newMesh.BeginPolygon(); #bottom
-        newMesh.AddPolygon(0);
-        newMesh.AddPolygon(3);
-        newMesh.AddPolygon(2);
-        newMesh.AddPolygon(1);
-        newMesh.EndPolygon();
-        newMesh.BeginPolygon(); #top
-        newMesh.AddPolygon(4);
-        newMesh.AddPolygon(5);
-        newMesh.AddPolygon(6);
-        newMesh.AddPolygon(7);
-        newMesh.EndPolygon();    
-        newMesh.BeginPolygon(); #front
-        newMesh.AddPolygon(0);
-        newMesh.AddPolygon(4);
-        newMesh.AddPolygon(7);
-        newMesh.AddPolygon(3);
-        newMesh.EndPolygon();    
-        newMesh.BeginPolygon(); #back
-        newMesh.AddPolygon(1);
-        newMesh.AddPolygon(2);
-        newMesh.AddPolygon(6);
-        newMesh.AddPolygon(5);
-        newMesh.EndPolygon();    
-        newMesh.BeginPolygon(); #right
-        newMesh.AddPolygon(0);
-        newMesh.AddPolygon(1);
-        newMesh.AddPolygon(5);
-        newMesh.AddPolygon(4);
-        newMesh.EndPolygon();    
-        newMesh.BeginPolygon(); #left
-        newMesh.AddPolygon(2);
-        newMesh.AddPolygon(3);
-        newMesh.AddPolygon(7);
-        newMesh.AddPolygon(6);
-        newMesh.EndPolygon();
+    #Copy the vertices in the mesh
+    newMesh.InitControlPoints(8)
+    newMesh.SetControlPointAt(FbxVector4(width, 0.0, depth, 0.0), 0)
+    newMesh.SetControlPointAt(FbxVector4(width, 0.0, -depth, 0.0), 1)
+    newMesh.SetControlPointAt(FbxVector4(-width, 0.0, -depth, 0.0), 2)
+    newMesh.SetControlPointAt(FbxVector4(-width, 0.0, depth, 0.0), 3)
+    newMesh.SetControlPointAt(FbxVector4(width, height, depth, 0.0), 4)
+    newMesh.SetControlPointAt(FbxVector4(width, height, -depth, 0.0), 5)
+    newMesh.SetControlPointAt(FbxVector4(-width, height, -depth, 0.0), 6)
+    newMesh.SetControlPointAt(FbxVector4(-width, height, depth, 0.0), 7)
+    newMesh.BeginPolygon(); #bottom
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(3);
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(1);
+    newMesh.EndPolygon();
+    newMesh.BeginPolygon(); #top
+    newMesh.AddPolygon(4);
+    newMesh.AddPolygon(5);
+    newMesh.AddPolygon(6);
+    newMesh.AddPolygon(7);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #front
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(4);
+    newMesh.AddPolygon(7);
+    newMesh.AddPolygon(3);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #back
+    newMesh.AddPolygon(1);
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(6);
+    newMesh.AddPolygon(5);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #right
+    newMesh.AddPolygon(0);
+    newMesh.AddPolygon(1);
+    newMesh.AddPolygon(5);
+    newMesh.AddPolygon(4);
+    newMesh.EndPolygon();    
+    newMesh.BeginPolygon(); #left
+    newMesh.AddPolygon(2);
+    newMesh.AddPolygon(3);
+    newMesh.AddPolygon(7);
+    newMesh.AddPolygon(6);
+    newMesh.EndPolygon();
     
-        #Add the mesh to the node
-        newNode.SetNodeAttribute(newMesh)
+    #Add the mesh to the node
+    newNode.SetNodeAttribute(newMesh)
 
-        return newNode
+    return newNode
